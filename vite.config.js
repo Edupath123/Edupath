59bbddfc64006c4e0cee2be34f584b9a4122ff2f
Edupath123 <edupath.officialapp@gmail.com>
2025-06-01 23:10:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // exposes on 0.0.0.0
-    port: process.env.PORT || 5173, // uses Render's assigned port or fallback
+    host: true,
+    port: process.env.PORT || 5173,
+    allowedHosts: ['edupath-eewi.onrender.com'], // 👈 Add this line
   },
 })
